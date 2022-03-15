6069dd76adbf30d20000a4ce4a002d4c2a69ede5
Icaro Apolo <apoloraci@gmail.com>
2022-03-15 19:22:22 -0300

    portfolio done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,32 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:(12)98143-4005">(12)98143-4005</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:apoloraci@gmail.com">apoloraci@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project a time</Slogan>
+        </CompanyContainer>
+        <SocialIcons href="https://github.com/IcaroApoloBR">
+          <AiFillGithub size="3rem" />  
+        </SocialIcons>
+        <SocialIcons href="https://www.linkedin.com/in/icaroapolo/">
+          <AiFillLinkedin size="3rem" />  
+        </SocialIcons>
+        <SocialIcons href="https://www.instagram.com/icaro.apolo/">
+          <AiFillInstagram size="3rem" />  
+        </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
